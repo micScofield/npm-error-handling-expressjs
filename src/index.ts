e071d74a1193e2556c2a67e18fd867ddc02ba53f
Sanyam Jain <sanyamjaino98@gmail.com>
2021-06-16 02:34:30 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ export * from './events/ticket-created-event'
 export * from './events/ticket-updated-event'
 export * from './events/order-created-event'
 export * from './events/order-cancelled-event'
+export * from './events/expiration-complete-event'
 
 // Order status Enum
 export * from './events/types/order-status'
